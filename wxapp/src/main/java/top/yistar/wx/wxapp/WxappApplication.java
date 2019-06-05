e82b5c4cdfbebda48dc553e164c51f0fcd45c311
chic <924204495@qq.com>
2019-06-05 17:54:09 +0800

    备注 主要看一下github上的备注 效果

--- wxapp/src/main/java/top/yistar/wx/wxapp/WxappApplication.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/WxappApplication.java
@@ -1,5 +1,7 @@
 package top.yistar.wx.wxapp;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -10,6 +12,11 @@ public class WxappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WxappApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("--------------------->>>>>>>springboot启动");
 	}
 
 }
