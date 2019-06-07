4dfda1030cfbfe9c15a9cc2c58e24ff996587b93
chic <924204495@qq.com>
2019-06-07 22:52:18 +0800

    修改springboot启动main方法

--- wxapp/src/main/java/top/yistar/wx/wxapp/WxappApplication.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/WxappApplication.java
@@ -1,7 +1,5 @@
 package top.yistar.wx.wxapp;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -12,11 +10,6 @@ public class WxappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WxappApplication.class, args);
-	}
-	
-	@PostConstruct
-	public void init() {
-		System.out.println("--------------------->>>>>>>springboot启动");
 	}
 
 }
