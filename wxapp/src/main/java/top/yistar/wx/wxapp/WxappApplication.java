dc9dd1ee81f46f18f64cb1c927312bf190a00d5e
chic <924204495@qq.com>
2019-06-19 16:35:32 +0800

    提交代码 测试 提交代码至github 触发jenkins自动部署的操作--

--- wxapp/src/main/java/top/yistar/wx/wxapp/WxappApplication.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/WxappApplication.java
@@ -1,15 +1,27 @@
 package top.yistar.wx.wxapp;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import top.yistar.wx.wxapp.util.LogUtil;
+
 @SpringBootApplication
 @ServletComponentScan
 public class WxappApplication {
+	
+	private static final Logger LOG = LogUtil.getLog(WxappApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(WxappApplication.class, args);
+	}
+	@PostConstruct
+	public void serverInit() {
+		
+		LOG.info("------------------------------服务器启动  测试 jenkins自动部署功能-----------");
 	}
 
 }
