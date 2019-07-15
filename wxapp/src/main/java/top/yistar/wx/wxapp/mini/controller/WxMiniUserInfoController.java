98a7558514b62b089280a382056b1181a5985ffc
chic <924204495@qq.com>
2019-07-15 16:21:18 +0800

    cc

--- wxapp/src/main/java/top/yistar/wx/wxapp/mini/controller/WxMiniUserInfoController.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/mini/controller/WxMiniUserInfoController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
