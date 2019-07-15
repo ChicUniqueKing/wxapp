8d6015f1d0205a5b009bd2ed7ec06a0d23d59c7f
chic <924204495@qq.com>
2019-07-15 16:08:22 +0800

    CC

--- wxapp/src/main/java/top/yistar/wx/wxapp/mini/controller/WxMiniUserInfoController.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/mini/controller/WxMiniUserInfoController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.wxpay.sdk.WXPay;
@@ -24,7 +25,7 @@ public class WxMiniUserInfoController {
 		return null;
 	}
 
-	@PostMapping(value="/minipay")
+	@RequestMapping(value="/minipay")
 	public ResponsePlafe payment(HttpServletRequest req,String productNo) {
 		
 		if(!StringUtils.hasText(productNo)) {
