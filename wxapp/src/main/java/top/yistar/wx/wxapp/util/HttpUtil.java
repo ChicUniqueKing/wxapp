5d361edd0b6610e6fd5830402e8d1fb43cd55b15
chic <924204495@qq.com>
2019-07-24 15:11:50 +0800

    qq登录回调

--- wxapp/src/main/java/top/yistar/wx/wxapp/util/HttpUtil.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/util/HttpUtil.java
@@ -1,123 +1,247 @@
 package top.yistar.wx.wxapp.util;
 
-import java.io.BufferedInputStream;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.nullValue;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.httpclient.HttpClient;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 
+import com.alibaba.fastjson.JSON;
+
 public class HttpUtil {
-	
-	
-	
-	public static String PostMap(String  url,Map<String, String> map) {
-		
+
+	private static CloseableHttpClient httpClient;
+	static {
+		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+		cm.setMaxTotal(100);
+		cm.setDefaultMaxPerRoute(20);
+		cm.setDefaultMaxPerRoute(50);
+		httpClient = HttpClients.custom().setConnectionManager(cm).build();
+	}
+
+	public static String PostMap(String url, Map<String, String> map) {
+
 		String responseContent = null;
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpPost post = new HttpPost(url);
 		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-		for(Map.Entry<String, String> entry:map.entrySet()) {
+		for (Map.Entry<String, String> entry : map.entrySet()) {
 			pairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
 		}
-		CloseableHttpResponse response =null;
-		HttpEntity entity =null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
 		try {
 			post.setEntity(new UrlEncodedFormEntity(pairs, "UTF-8"));
-				response =httpClient.execute(post);
-				if(response !=null && response.getStatusLine().getStatusCode()==200) {
-					entity = response.getEntity();
-					responseContent = EntityUtils.toString(entity, "UTF-8");
-					Logger logger = LogUtil.getLog(HttpUtil.class);
-					logger.info("postMap 调用结果：{}",responseContent.toString());
-				}
-				return responseContent;
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}finally {
-				
-				if(httpClient!=null) {
-					try {
-						httpClient.close();
-						if(response!=null) {
-							response.close();
-						}
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
+			response = httpClient.execute(post);
+			if (response != null && response.getStatusLine().getStatusCode() == 200) {
+				entity = response.getEntity();
+				responseContent = EntityUtils.toString(entity, "UTF-8");
+				Logger logger = LogUtil.getLog(HttpUtil.class);
+				logger.info("postMap 调用结果：{}", responseContent.toString());
 			}
-		return null;
-	}
-	
-	
-	public static String invokeRemote(String url,String reqType,Map<String, String> reqData) {
-		
-		InputStream in = null;
-		InputStreamReader inputStreamReader = null;
-		BufferedReader reader = null;
-		try {
-			URL Uri = new URL(url);
-			HttpURLConnection connection =(HttpURLConnection) Uri.openConnection();
-			//connection.setRequestProperty(reqData.keySet().);
-			connection.connect();
-			//connection.setRequestMethod(reqType);
-			in = connection.getInputStream();
-			inputStreamReader = new InputStreamReader(in);
-			reader = new BufferedReader(inputStreamReader);
-			String line ="";
-			StringBuffer buffer = new StringBuffer();
-			while((line=reader.readLine())!=null) {
-				buffer.append(line);
-			}
-			
-			return buffer.toString();
+			return responseContent;
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
-			
+		} finally {
+
+			if (httpClient != null) {
 				try {
-					if(reader!=null)reader.close();
-					if(inputStreamReader!=null)inputStreamReader.close();
-					if(reader!=null)reader.close();
+					httpClient.close();
+					if (response != null) {
+						response.close();
+					}
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-		
-		
+		}
 		return null;
-		
 	}
-	
-	
-	
-	
-	
+
+	/**
+	 * 
+	 * @param url     请求url
+	 * @param reqType 请求方式
+	 * @param reqData 请求数据
+	 * @return
+	 */
+	public static String invokeRemote(String url, String reqType, Map<String, String> reqData) {
+
+		InputStream in = null;
+		InputStreamReader inputStreamReader = null;
+		BufferedReader reader = null;
+		HttpURLConnection connection = null;
+		OutputStream out = null;
+		try {
+			URL Uri = new URL(url);
+			connection = (HttpURLConnection) Uri.openConnection();
+			connection.setConnectTimeout(50000);// 超时时间
+			connection.setRequestMethod(reqType);// 请求方式
+			//connection.setRequestProperty("Content-type", "application/json; charset=utf-8");
+			//connection.setRequestProperty("Accept", "application/json");
+			connection.setUseCaches(false);
+			connection.setInstanceFollowRedirects(true);
+			// 设置请求参数
+			/*
+			 * for(String key:reqData.keySet()) { connection.addRequestProperty(key,
+			 * reqData.get(key)); }
+			 */
+			// System.out.println(connection.getRequestProperty("name"));
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			connection.connect();
+			// 获取输出流
+			out = connection.getOutputStream();
+			DataOutputStream outs = new DataOutputStream(out);
+			// 输出流里写入POST参数
+			/*StringBuffer buffer1 = new StringBuffer();
+			for (String key : reqData.keySet()) {
+				buffer1.append(key + "=" + reqData.get(key));
+			}*/
+			outs.writeBytes(JSON.toJSONString(reqData));
+			outs.flush();
+			outs.close();
+			in = connection.getInputStream();
+			inputStreamReader = new InputStreamReader(in);
+			reader = new BufferedReader(inputStreamReader);
+			String line = "";
+			StringBuffer buffer = new StringBuffer();
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line);
+			}
+
+			return buffer.toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null)
+					reader.close();
+				if (inputStreamReader != null)
+					inputStreamReader.close();
+				if (reader != null)
+					reader.close();
+				// if(connection!=null)connection.disconnect();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+
+	}
+
+	public static String getInvoke(String url, Map<String, String> map) {
+
+		HttpGet httpGet = new HttpGet();
+		httpGet.addHeader("Content-Type", "application/Json; charset=UTF-8");
+		try {
+			httpGet.setURI(new URI(url));
+			BasicHttpParams params = new BasicHttpParams();
+			for (String key : map.keySet()) {
+				params.setParameter(key, map.get(key));
+			}
+			httpGet.setParams(params);
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			httpClient.execute(httpGet);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>();
+		map.put("name", "15850781443");
+		/*String json = JSON.toJSONString(map);
+		System.out.println("=============??data"+json);
+		String str = invokeRemote("http://localhost:8888/test", "POST", map);
+		System.out.println(str);*/
+	    /*String rs = post("http://localhost:8888/test",json);
+	    System.out.println(rs);*/
+		// getInvoke("http://localhost:8888/test",map);
+		String rs = post("http://localhost:9990/test", JSON.toJSONString(map));
+		System.out.println(rs);
+	}
+
+	public static String post(String url, String jsonString) {
+		CloseableHttpResponse response = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			HttpPost httpPost = new HttpPost(url);
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000)
+					.setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+			httpPost.setConfig(requestConfig);
+			httpPost.setConfig(requestConfig);
+			httpPost.addHeader("Content-type", "application/json; charset=utf-8");
+			httpPost.setHeader("Accept", "application/json");
+			httpPost.setEntity(new StringEntity(jsonString, Charset.forName("UTF-8")));
+			response = httpClient.execute(httpPost);
+			in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			//String NL = System.getProperty("line.separator");
+			//System.out.println("==========================================================="+NL);
+			while ((line = in.readLine()) != null) {
+				sb.append(line);
+			}
+			in.close();
+			result = sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != response) {
+					response.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 
 }
--- wxapp/src/main/java/top/yistar/wx/wxapp/wxcontroller/ThreedGroupLoginController.java
+++ wxapp/src/main/java/top/yistar/wx/wxapp/wxcontroller/ThreedGroupLoginController.java
@@ -1,27 +1,106 @@
 package top.yistar.wx.wxapp.wxcontroller;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import ch.qos.logback.classic.net.SyslogAppender;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import top.yistar.wx.wxapp.entity.ResponsePlafe;
+import top.yistar.wx.wxapp.util.HttpUtil;
 
 /**
  * 
  * @author ChicUniqueKing
- *  第三方登录控制器
+ *  第三方qq登录 回调
  */
+@Controller
 public class ThreedGroupLoginController {
+	
+	public static final Logger LOG = LoggerFactory.getLogger(ThreedGroupLoginController.class);
+	
+	
+	public static final String QQ_TOKEN_URL = "https://graph.qq.com/oauth2.0/token";
 	
 	/**
 	 *  step1: https://graph.qq.com/oauth2.0/authorize 获取认证code
 	 */
+	//https://www.yistar.top/callback?code=8D234BE8D947163E1CE0F772249E4BDE
 	
+	@RequestMapping(value="/")
+	public String loginForQQCallBack(HttpServletRequest request) {
+		String code = request.getParameter("code");
+		LOG.info("--=================>>qq登录回调------》》》");
+		/*if(!StringUtils.hasLength(code)) {
+			return new ResponsePlafe(003,"","获取qq登录回调code失败");
+			return "redirect:https://www.yistar.top/line.html?rs=failed";
+		}
+		try {
+		//根据code获取token
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("grant_type", "authorization_code");
+		params.put("client_id", "101684736");
+		params.put("client_secret", "6f2c34d716f7add016cbe94db7f103af");
+		params.put("code", code);
+		params.put("redirect_uri", "https://www.yistart.top");
+		//String rs =HttpUtil.postData(QQ_TOKEN_URL, JSON.toJSONString(params), "");
+		String rs = "";
+		LOG.info("------获取token-------"+rs);
+		if(!StringUtils.hasLength(rs)) {
+			return "redirect:https://www.yistar.top/line.html?rs=failed";
+		}
+		JSONObject jObject = new JSONObject();
+		jObject.parseObject(rs);
+		String token = jObject.getString("access_token");
+		Map<String, String> returnData = new HashMap<>();
+		returnData.put("access_token", token);//qq登录token
+		returnData.put("redirect_uri", "https://www.yistart.top");
+		return new ResponsePlafe(001,"success",returnData);
+		return "redirect:https://www.yistar.top/line.html?rs=failed";
+		}catch(Exception e) {
+			LOG.info("调用qq获取token异常",e.getMessage());
+			//return new ResponsePlafe(004,"","调用qq获取token异常");
+			return "redirect:https://www.yistar.top";
+		}*/
+		
+		return "redirect:https://www.yistar.top";
+		
+		
+	}
 	
 	
 	
 	public static void main(String arg []) {
 		
-		System.out.println(new Date().getTime());
-		System.out.println(System.currentTimeMillis());
+		/*System.out.println(new Date().getTime());
+		System.out.println(System.currentTimeMillis());*/
+		
+		//根据code获取token
+				Map<String, String> params = new HashMap<String, String>();
+				params.put("grant_type", "authorization_code");
+				params.put("client_id", "101684736");
+				params.put("client_secret", "6f2c34d716f7add016cbe94db7f103af");
+				params.put("code", "B5AF6092F7C4C556BB7CB97F1747169C");
+				params.put("redirect_uri", "https://www.yistart.top");
+				StringBuffer buffer = new StringBuffer(QQ_TOKEN_URL);
+				
+				for(String key:params.keySet()) {
+					buffer.append("&").append(key).append("=").append(params.get(key));
+				}
+			String url = buffer.toString();
+			String url_n = url.trim().replaceFirst("&", "?");
+					LOG.info("=============>>>>>>>>"+url_n);
+				//String rs =	top.yistar.wx.wxapp.mini.pay.config.HttpUtil.postData(url_n, params.toString());
+				String rs = HttpUtil.invokeRemote(url_n, "GET", new HashMap<>());
+				LOG.info("------获取token-------"+rs);
 		
 	}
 	
