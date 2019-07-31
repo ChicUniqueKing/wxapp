2e03397a9ec45991c289ca6ef5eac9474d6dbc34
chic <924204495@qq.com>
2019-07-31 14:50:19 +0800

    远程接口调用异常类

---
+++ wxapp/src/main/java/top/yistar/wx/wxapp/exception/RquesetMethodException.java
@@ -0,0 +1,15 @@
+package top.yistar.wx.wxapp.exception;
+
+/**
+ * @Author chic
+ * @Date 2019/7/31 10 12
+ * @Description
+ * @Company 测试信息技术
+ * @Team chic项目组
+ */
+public class RquesetMethodException extends Throwable {
+
+    public RquesetMethodException(String msg) {
+
+    }
+}
