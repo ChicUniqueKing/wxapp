471fd244f8a19419f5d996eed1c8c13c8bb56d16
chic <924204495@qq.com>
2019-08-05 15:29:41 +0800

    更新

---
+++ wxapp/src/main/java/top/yistar/wx/wxapp/util/UserInfoManager.java
@@ -0,0 +1,20 @@
+package top.yistar.wx.wxapp.util;
+
+import top.yistar.wx.wxapp.entity.User;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Author chic
+ * @Date 2019/8/5 12 35
+ * @Description
+ * @Company 测试信息技术
+ * @Team chic项目组
+ */
+public class UserInfoManager {
+
+    public static  final Map<String, String> userInfoMap = new  ConcurrentHashMap<String, String>();
+
+
+}
