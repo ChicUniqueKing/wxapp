87aa16afa6ca79a6734a7dc593087885dc58ee1c
chic <924204495@qq.com>
2019-08-05 17:59:42 +0800

    socket常量

---
+++ wxapp/src/main/java/top/yistar/wx/wxapp/websocket/WebsocketConstant.java
@@ -0,0 +1,14 @@
+package top.yistar.wx.wxapp.websocket;
+
+/**
+ * @Author chic
+ * @Date 2019/8/2 16 02
+ * @Description
+ * @Company 测试信息技术
+ * @Team chic项目组
+ */
+public class WebsocketConstant {
+
+    public static final String SYSTEM_ID = "0000";
+
+}
